7c00beb14b765270b857b3f638e072a7359f7085
Cloyd Arvie G. Floremonte <s.cagfloremonte@usm.edu.ph>
2026-09-14 23:51:53 +0800

    feat: add Django model forms for staff panel management

--- staff_panel/forms.py
+++ staff_panel/forms.py
@@ -36,7 +36,7 @@ class LaptopAdminForm(forms.ModelForm):
         self.fields['slug'].required = False
         self.fields['main_image'].widget = forms.FileInput(attrs={'class': 'sp-file-input'})
         self.fields['gallery_images'] = forms.FileField(
-            widget=forms.ClearableFileInput(attrs={'multiple': True, 'class': 'sp-file-input'}),
+            widget=forms.FileInput(attrs={'multiple': True, 'class': 'sp-file-input'}),
             required=False,
             help_text="Select multiple images to add to the gallery."
         )
